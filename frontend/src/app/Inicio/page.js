d85a492bfd2b63b51ea0827713d9eca803c34aff
Raijam13 <bustiosterresabraham@gmail.com>
2023-09-07 18:24:43 -0500

    d

--- frontend/src/app/Inicio/page.js
+++ frontend/src/app/Inicio/page.js
@@ -1,9 +1,17 @@
 'use client';
+
+import barra from '../../components/barra/barra.jsx'
 import styles from './styles.module.css'
 
 const inicio = () => {
-    return(
+    return( 
         <div className={styles.fondo}>
+        
+            <barra></barra>
+
+            <div className={styles.contenido}>
+
+            </div>
 
         </div>
 
---
+++ frontend/src/components/barra/barra.jsx
@@ -0,0 +1,17 @@
+'use client';
+import styles from './styles.module.css'
+import React, {Component} from 'react';
+
+const barra = () => { 
+    return(
+        <div  className={styles.fondo} >
+
+                <h1>Hola</h1>
+                <div className={styles.home}>
+                    
+                </div>
+        </div>
+    );
+};
+
+export default barra;
