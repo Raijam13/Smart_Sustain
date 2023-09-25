b6b20672ad8ec9984e9b98de5e272f0581ffd732
Raijam13 <bustiosterresabraham@gmail.com>
2023-09-25 17:49:07 -0500

    Registro

---
+++ frontend/src/app/Registro/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+import styles from './styles.module.css'
+import React from 'react';
+import Barra from '../../components/barra/barra';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Registro = () => {
+    return( 
+        <div className={styles.fondo}>
+         <div className={styles.cointainer}>
+
+            <h1>Registro</h1>
+
+            <div className={styles.columna}>
+                 <div className={styles.items}>
+                     <div className={styles.label}><label>Usuario</label> </div>
+                     <Form.Control id='inputs' type="text" size="sm" placeholder="" className={styles.inputs} />
+                 </div>
+
+                <div className={styles.items}>
+                 <div className={styles.label}><label>Contraseña</label> </div>
+                    <Form.Control type="text" size="sm" placeholder="" className={styles.inputs} />
+                </div>
+            </div>
+
+            <div className={styles.columna}>
+                 <div className={styles.items}>
+                     <div className={styles.label}><label>Nombres</label> </div>
+                     <Form.Control id='inputs' type="text" size="sm" placeholder="" className={styles.inputs} />
+                 </div>
+
+                <div className={styles.items}>
+                 <div className={styles.label}><label>Apellidos</label> </div>
+                    <Form.Control type="text" size="sm" placeholder="" className={styles.inputs} />
+                </div>
+            </div>
+            
+         </div>
+        </div>
+
+    
+    )
+}
+
+export default Registro;
