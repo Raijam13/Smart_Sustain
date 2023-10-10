d5b07d99d46c6330dcd7957f8b57e018009fe62c
Sergio <sergio.moy@gmail.com>
2023-10-09 21:06:06 -0500

    avances en mostrar balance

--- frontend/src/app/Movimientos/page.tsx
+++ frontend/src/app/Movimientos/page.tsx
@@ -5,9 +5,9 @@ import Barra from '../../components/barra/barra';
 
 const inicio = () => {
     
-    let tabla = [<tr><td>Comida</td><td>30</td><td>Almuerzo en restaurante</td></tr>,
-                <tr><td>Transporte</td><td>5</td><td>Pasajes 05/10</td></tr>,
-                <tr><td>Entretenimiento</td><td>15</td><td>Suscripcion netflix</td></tr>]
+    let tabla = [<tr><td>Comida</td><td>30</td><td>Almuerzo en restaurante</td><td>05/10 13:00</td></tr>,
+                <tr><td>Transporte</td><td>5</td><td>Pasajes</td><td>05/10 11:00</td></tr>,
+                <tr><td>Entretenimiento</td><td>15</td><td>Suscripcion netflix</td><td>03/10 22:18</td></tr>]
     return( 
         <div className={styles.fondo}>
         
@@ -16,11 +16,12 @@ const inicio = () => {
         <h2>Mi balance</h2>
         <p><b>$123</b></p>
         <h2>Lista de movimientos</h2>
-        <table>
+        <table className={styles.tabla}>
             <tr>
                 <th>Categoria</th>
                 <th>Cantidad</th>
                 <th>Descripcion</th>
+                <th>Fecha</th>
             </tr>
             {tabla}
         </table></div>
