295c4f9b09cccc8a2aad863375ad273e0f657fb1
Raijam13 <bustiosterresabraham@gmail.com>
2023-10-12 12:45:43 -0500

    verifación inputs Login Registro

--- frontend/src/app/Login/page.js
+++ frontend/src/app/Login/page.js
@@ -4,9 +4,30 @@ import logo from '../../../logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 
 const login = () => {
+
+    const [correo, setCorreo] = useState('');
+    const [contra, setContra] = useState('');
+
+
+    const isEmpty = (str) => {
+        return str.trim() === '';
+      };
+
+    const handleValidar = () => {
+        if (isEmpty(correo) || isEmpty(contra)) {
+          alert('Porfavor ingrese unas credenciales validas');
+        } else {
+          // Realiza la lógica de validación o cualquier otra acción aquí
+          console.log('Correo:', correo);
+          console.log('Contraseña:', contra);
+        }
+      };
+
+
     return(
         <div className={styles.fondo}>
             <div className={styles.form}>
@@ -15,13 +36,13 @@ const login = () => {
                 </div>
                 <div className={styles.logo}></div>
                 <div className={styles.container}>  
-                      <Form.Control type="text" placeholder="Correo" className={styles.inputs} />
+                      <Form.Control type="text" placeholder="Correo" className={styles.inputs} onChange={(e) => setCorreo(e.target.value)} />
 
-                      <Form.Control type="text" placeholder="Contraseña" className={styles.inputs} />
+                      <Form.Control type="text" placeholder="Contraseña" className={styles.inputs} onChange={(e) => setContra(e.target.value)} />
                 </div>
                 
                 <div className={styles.boton}>
-                     <Button variant="warning">Warning</Button>{' '}
+                     <Button variant="warning" onClick={handleValidar}>Warning</Button>{' '}
                      
                 </div>
             </div>
