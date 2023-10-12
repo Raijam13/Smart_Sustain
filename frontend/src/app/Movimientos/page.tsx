af61d6278bb66395d91d8ddbb077d772cd76f901
Sergio <sergio.moy@gmail.com>
2023-10-11 20:52:27 -0500

    conexion back-front movimientos

--- frontend/src/app/Movimientos/page.tsx
+++ frontend/src/app/Movimientos/page.tsx
@@ -1,26 +1,26 @@
-'use client';
+
 import styles from './styles.module.css'
 import React from 'react';
 import Barra from '../../components/barra/barra';
-
-const inicio = () => {
+import MovApi from '../../api/movimientos.js';
     
-    let tabla = [<tr><td>Comida</td><td>30</td><td>Almuerzo en restaurante</td><td>05/10 13:00</td></tr>,
-                <tr><td>Transporte</td><td>5</td><td>Pasajes</td><td>05/10 11:00</td></tr>,
-                <tr><td>Entretenimiento</td><td>15</td><td>Suscripcion netflix</td><td>03/10 22:18</td></tr>]
+
+const movimientos = () => {
+    
+    let tabla = MovApi.peticion()
     return( 
         <div className={styles.fondo}>
         
         <Barra></Barra>
         <div><h1 className={styles.titulo}>Movimientos</h1>
-        <h2>Mi balance</h2>
+        <h2>Gastos netos este mes</h2>
         <p><b>$123</b></p>
         <h2>Lista de movimientos</h2>
         <table className={styles.tabla}>
             <tr>
+                <th>Persona</th>
                 <th>Categoria</th>
                 <th>Cantidad</th>
-                <th>Descripcion</th>
                 <th>Fecha</th>
             </tr>
             {tabla}
@@ -31,4 +31,4 @@ const inicio = () => {
     )
 }
 
-export default inicio;
+export default movimientos;
