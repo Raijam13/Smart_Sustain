8a5fd427573c69706116e5ed91a8f1cadcfa3cf8
Sergio <sergio.moy@gmail.com>
2023-10-12 15:16:26 -0500

    endpoint validar login

--- backend/smartsustain/urls.py
+++ backend/smartsustain/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("movimientos", views.obtenermovimientos)
+    path("movimientos", views.obtenermovimientos),
+    path("login", views.validarlogin)
 ]
--- backend/smartsustain/views.py
+++ backend/smartsustain/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+wrongtype = "Tipo de petición no soportado"
 
 @csrf_exempt
 def obtenermovimientos(request):
-    lista = []
-    data = json.loads(request.body)
-    user = data["usuario"]
     if request.method == "POST":
+        lista = []
+        data = json.loads(request.body)
+        user = data["usuario"]
         MovimientosQuerySet = Movimiento.objects.filter(usuario = user)
         for m in MovimientosQuerySet:
             lista.append({
@@ -21,3 +22,26 @@ def obtenermovimientos(request):
             })
         dict = {"lista" : lista}
         return HttpResponse(json.dumps(dict))
+    else:
+        return HttpResponse(wrongtype)
+    
+@csrf_exempt
+def validarlogin(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        email = data["email"]
+        passw = data["password"]
+        UsuariosQuerySet = Usuario.objects.all()
+        resp = {"resp" : "no_user"}
+        for u in UsuariosQuerySet:
+            if u.email == email:
+                if u.password == passw:
+                    resp["resp"] = "login_ok"
+                else:
+                    resp["resp"] = "wrong_password"
+                break
+        return HttpResponse(json.dumps(resp))
+
+    else:
+        return HttpResponse(wrongtype)
+    
