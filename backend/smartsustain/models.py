88e7c7dd1b97be6ad472eede330b4ce287b590a9
Sergio <sergio.moy@gmail.com>
2023-11-20 19:55:23 -0500

    backends sprint 2

--- backend/smartsustain/models.py
+++ backend/smartsustain/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-'''class Familia(models.Model):
+class Familia(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
 
     def __str__(self):
         return self.nombre
-'''
+
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=255)
@@ -17,21 +17,21 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nombre + " " + self.apellido
     
-'''class Notificacion(models.Model):
+class Notificacion(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
 
     def __str__(self):
         return self.nombre
-'''
-'''class Categoria(models.Model):
+
+class Categoria(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nombre + " - " + self.usuario.nombre + " " + self.usuario.apellido
-'''
+
           
 class Movimiento(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
@@ -42,7 +42,7 @@ class Movimiento(models.Model):
     def __str__(self) :
         return self.usuario.nombre + " " + self.usuario.apellido + " " + str(self.fecha)
     
-'''class Objetivo(models.Model):
+class Objetivo(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete= models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE)
     nombre = models.CharField(max_length=255)
@@ -53,13 +53,11 @@ class Movimiento(models.Model):
     
     def __str__(self):
         return self.nombre
-'''
-    
-'''class UsuarioXFamilia(models.Model):
+
+class UsuarioXFamilia(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     familia = models.ForeignKey(Familia, on_delete=models.CASCADE)
-    admin = models.BooleanField()
+    admin = models.BooleanField(default = False)
 
     def __str__(self):
         return self.usuario.nombre + " - " + self.familia.nombre
-        '''
